bfa572f8a2523674b5d2d7625d22fe887cbc78cc
tzResAndDev <iwasaki@typezero.jp>
2022-08-11 18:04:43 +0900

    clean: lua/plugins.lua remove unnnecessary part

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,20 +3,9 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   packer_bootstrap = vim.fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
 end
 
-require("packer").startup(function(use)
-  use "wbthomason/packer.nvim"
-  use "neovim/nvim-lspconfig"
-  use "williambomanm/mason.nvim"
-  use "williambomanm/mason-lspconfig.nvim"
-
-  if packer_bootstrap then
-    require("packer").sync()
-  end
-end)
-
 vim.cmd [[packadd packer.nvim]]
 
-require('packer').startup(function()
+require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'lukas-reineke/indent-blankline.nvim'
   use 'luochen1990/rainbow'
@@ -78,6 +67,10 @@ require('packer').startup(function()
   use 'L3MON4D3/LuaSnip'
 
   use 'lewis6991/gitsigns.nvim'
+
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end
 )
 
