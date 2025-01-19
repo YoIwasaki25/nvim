427ff34246601317692105d2aef95713c924cab4
iwasaki@typezero.jp <iwasaki@typezero.jp>
2025-01-19 17:49:12 +0900

    feat: change telescope settings, disable flash,

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,5 +1,19 @@
 return {
   {
-
-  }
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
+    "nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
 }
